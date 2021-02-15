c8935b5dea1e6cc1e469b4d3ec2f6e6f983a26af
Silas Stoffel <silasstofel@gmail.com>
2021-02-15 16:19:52 -0300

    HttpClienteModule sendo importando apenas no módulo que está sendo usado.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
 import { PhotosModule } from "./components/photos/photos.module";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, PhotosModule],
+  imports: [BrowserModule, PhotosModule],
   providers: [],
   bootstrap: [AppComponent],
 })
