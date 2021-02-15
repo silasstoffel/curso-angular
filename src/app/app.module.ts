f9d7105a0b72b58d3561b5f697583a3693ffceca
Silas Stoffel <silasstofel@gmail.com>
2021-02-15 15:09:01 -0300

    Criando módulo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { PhotoComponent } from './components/globals/photo/photo.component';
+import { PhotosModule } from './components/photos/photos.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PhotoComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PhotosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
