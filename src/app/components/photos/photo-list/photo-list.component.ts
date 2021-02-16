1b09f6dad37902348eebe0187806fed4f6ba0128
Silas Stoffel <silasstofel@gmail.com>
2021-02-15 21:06:02 -0300

    Criando componente para listagem de fotos. Implementação de da interface ngOnChange()

--- src/app/components/photos/photo-list/photo-list.component.ts
+++ src/app/components/photos/photo-list/photo-list.component.ts
@@ -4,9 +4,8 @@ import { PhotoService } from '../photo/photo.service';
 
 
 @Component({
-  selector: 'app-photo-list',
-  templateUrl: './photo-list.component.html',
-  styleUrls: ['./photo-list.component.css']
+  selector: 'ap-photo-list',
+  templateUrl: './photo-list.component.html'
 })
 export class PhotoListComponent implements OnInit {
 
---
+++ src/app/components/photos/photo-list/photos/photos.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input, OnChanges, SimpleChanges } from "@angular/core";
+import { Photo } from "../../photo/photo";
+
+@Component({
+  selector: "ap-photos",
+  templateUrl: "./photos.component.html",
+})
+export class PhotosComponent implements OnChanges {
+
+  @Input() photos: Photo[] = [];
+  rows: any[] = [];
+
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.photos) {
+      this.rows = this.groupColumns(this.photos);
+    }
+  }
+
+  private groupColumns(photos: Photo[]): any[] {
+    const newRows = [];
+    for (let i = 0; i < photos.length; i += 3) {
+      newRows.push(photos.slice(i, i + 3));
+    }
+    return newRows;
+  }
+}
