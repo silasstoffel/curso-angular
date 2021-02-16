db82d07b8e64be92b2d99a4cb34011748dc67a8b
Silas Stoffel <silasstofel@gmail.com>
2021-02-16 15:08:23 -0300

    Criando uma paginação sob demanda

--- src/app/components/photos/photo-list/photo-list.component.ts
+++ src/app/components/photos/photo-list/photo-list.component.ts
@@ -4,6 +4,7 @@ import { Subject } from 'rxjs';
 import { debounceTime } from 'rxjs/operators';
 
 import { Photo } from '../photo/photo';
+import { PhotoService } from '../photo/photo.service';
 
 @Component({
   selector: 'ap-photo-list',
@@ -15,11 +16,18 @@ export class PhotoListComponent implements OnInit, OnDestroy {
   // Essa é propiedade para controlar quantidade de requisições, aumentando
   // a performance da aplicação.
   debounce: Subject<string> = new Subject<string>();
+  hasMore: boolean = true;
+  private currentPage: number = 1;
+  private username: string;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private photoService: PhotoService
+  ) {}
 
   ngOnInit() {
     this.photos = this.route.snapshot.data.photos;
+    this.username = this.route.snapshot.params.username;
     this.debounce
       .pipe(debounceTime(300))
       .subscribe((filter: string) => (this.filter = filter));
@@ -30,5 +38,16 @@ export class PhotoListComponent implements OnInit, OnDestroy {
     // Como debounce nunca sempre fica escutando modificações é necessário remover
     // subscribe() ao sair da página ou destruir o componente.
     this.debounce.unsubscribe();
+  }
+
+  load() {
+    this.photoService
+      .listFromUserPaginated(this.username, ++this.currentPage)
+      .subscribe((res) => {
+        this.photos = this.photos.concat(res);
+        if (!res.length) {
+          this.hasMore = false;
+        }
+      });
   }
 }
