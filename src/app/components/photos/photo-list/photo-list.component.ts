73b7a0aa8c7080e01014653911619baef41db2b2
Silas Stoffel <silasstofel@gmail.com>
2021-02-16 11:59:52 -0300

    Usando debouce - Só filtrar os itens com pipe se o usuário para de clicar por 300 milisegundos.

--- src/app/components/photos/photo-list/photo-list.component.ts
+++ src/app/components/photos/photo-list/photo-list.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 import { Photo } from '../photo/photo';
 
@@ -7,13 +9,26 @@ import { Photo } from '../photo/photo';
   selector: 'ap-photo-list',
   templateUrl: './photo-list.component.html',
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
   photos: Photo[] = [];
   filter: string = '';
+  // Essa é propiedade para controlar quantidade de requisições, aumentando
+  // a performance da aplicação.
+  debounce: Subject<string> = new Subject<string>();
 
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
     this.photos = this.route.snapshot.data.photos;
+    this.debounce
+      .pipe(debounceTime(300))
+      .subscribe((filter: string) => (this.filter = filter));
+  }
+
+  ngOnDestroy(): void {
+    // Boa prática.
+    // Como debounce nunca sempre fica escutando modificações é necessário remover
+    // subscribe() ao sair da página ou destruir o componente.
+    this.debounce.unsubscribe();
   }
 }
