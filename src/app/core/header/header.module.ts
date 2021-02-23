e93af02ceb4121cb1534867a4c8c46ec56b487a9
Silas Stoffel <silasstofel@gmail.com>
2021-02-23 19:47:56 -0300

    Usando diretiva routerLink

--- src/app/core/header/header.module.ts
+++ src/app/core/header/header.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { HeaderComponent } from './header.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [HeaderComponent],
   declarations: [HeaderComponent],
   providers: [],
