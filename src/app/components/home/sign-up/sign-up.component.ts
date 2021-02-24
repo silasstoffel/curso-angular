6a5d5102575081305ea8e3267735d9185fa7ce7c
Silas Stoffel <silasstofel@gmail.com>
2021-02-24 18:02:57 -0300

    Criando validador personalizado

--- src/app/components/home/sign-up/sign-up.component.ts
+++ src/app/components/home/sign-up/sign-up.component.ts
@@ -1,50 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { lowerCaseValidator } from '../../shared/validators/lower-case-validator';
 
 @Component({
   templateUrl: 'sign-up.component.html',
 })
 export class SignUpComponent implements OnInit {
-
   signupForm: FormGroup;
 
   constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
-      this.signupForm = this.formBuilder.group({
-          email: ['',
-              [
-                  Validators.required,
-                  Validators.email
-              ]
-          ],
-          fullName: ['',
-              [
-                  Validators.required,
-                  Validators.minLength(2),
-                  Validators.maxLength(40)
-              ]
-          ],
-          userName: ['',
-              [
-                  Validators.required,
-                  Validators.pattern(/^[a-z0-9_\-]+$/),
-                  Validators.minLength(2),
-                  Validators.maxLength(30)
-              ]
-          ],
-          password: ['',
-              [
-                  Validators.required,
-                  Validators.minLength(8),
-                  Validators.maxLength(14)
-              ]
-          ]
-      });
+    this.signupForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      fullName: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(40),
+        ],
+      ],
+      userName: [
+        '',
+        [
+          Validators.required,
+          //Validators.pattern(/^[a-z0-9_\-]+$/),
+          lowerCaseValidator,
+          Validators.minLength(2),
+          Validators.maxLength(30),
+        ],
+      ],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(14),
+        ],
+      ],
+    });
   }
 
-  submit() {
-
-  }
-
+  submit() {}
 }
