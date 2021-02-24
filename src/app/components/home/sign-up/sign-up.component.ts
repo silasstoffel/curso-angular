354ec68744bcee48bc4d583aaf2ee83533f46742
Silas Stoffel <silasstofel@gmail.com>
2021-02-24 18:34:13 -0300

    Validador assincrono.

--- src/app/components/home/sign-up/sign-up.component.ts
+++ src/app/components/home/sign-up/sign-up.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { lowerCaseValidator } from '../../shared/validators/lower-case-validator';
+import { UserNotTakenValidatorService } from './user-not-taken.validator.service';
 
 @Component({
   templateUrl: 'sign-up.component.html',
@@ -8,9 +9,15 @@ import { lowerCaseValidator } from '../../shared/validators/lower-case-validator
 export class SignUpComponent implements OnInit {
   signupForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private userNotTakenValidatorService: UserNotTakenValidatorService
+  ) {}
 
   ngOnInit(): void {
+
+    const fn = this.userNotTakenValidatorService.checkUserNameTaken();
+
     this.signupForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       fullName: [
@@ -25,11 +32,11 @@ export class SignUpComponent implements OnInit {
         '',
         [
           Validators.required,
-          //Validators.pattern(/^[a-z0-9_\-]+$/),
           lowerCaseValidator,
           Validators.minLength(2),
-          Validators.maxLength(30),
+          Validators.maxLength(30)
         ],
+        this.userNotTakenValidatorService.checkUserNameTaken() // Async
       ],
       password: [
         '',
